9ad675ae0f0d64c139a7e218ea6e94b491511aca
Edson Souza <edsonfrs@gmail.com>
2022-05-05 23:54:11 -0300

    Testando os Elementos: RadioButton, CheckBox e Combo

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,8 +1,12 @@
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCampoTreinamento {
 	
@@ -30,7 +34,72 @@ public class TesteCampoTreinamento {
 				
 		driver.quit();
 	}
+	
+	@Test
+	public void deveInteragirComRadioButton() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/camp/componentes.html");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		boolean resultado = driver.findElement(By.id("elementosForm:sexo:0")).isSelected();
+		
+		Assert.assertTrue(resultado);
+				
+		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComCheckBox() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/camp/componentes.html");
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		boolean resultado = driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected();
+		
+		Assert.assertTrue(resultado);
+				
+		driver.quit();
+	} 
+	
+	@Test
+	public void deveInteragirComCombo() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/camp/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		// Possibilidades de seleção de elementos no elemento Combo
+		//combo.selectByIndex(5);
+		//combo.selectByValue("mestrado");
+		combo.selectByVisibleText("Superior");
+		String selecao = combo.getFirstSelectedOption().getText();
+		
+		Assert.assertEquals("Superior", selecao);
+				
+		driver.quit();
+	}
 
+	@Test
+	public void deveVerificarvaloresCombo() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/camp/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		// Pegando a lista de opções do Combo
+		List<WebElement> options = combo.getOptions();
+		
+		// Pegando a quantidade opções no Combo
+		Assert.assertEquals(8, options.size());
+		
+		boolean encontrou = false;
+		for(WebElement option: options) {
+			if(option.getText().equals("Mestrado")) {
+				encontrou = true;
+				break;
+			}
+		}
+		
+		Assert.assertTrue(encontrou);
+				
+		driver.quit();
+	}
 	
 
 }
